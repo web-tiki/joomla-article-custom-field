6b9ec49deb07d6911e3cd8378d668b21d01ee109
web-tiki <contact@web-tiki.com>
2016-03-21 18:41:11 +0100

    Category selection is JS based

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # joomla-article-custom-field
-Joomla plugin to add a custom field to a selected category
+Joomla plugin to add a custom field to a selected category.
+This plugin was created after [this question](http://joomla.stackexchange.com/questions/15764/article-custom-fields-for-one-category/15766?noredirect=1#comment19963_15766) on joomla.stackexchange.com.
+
+##Instalation
+1. [download ZIP](https://github.com/web-tiki/joomla-article-custom-field/archive/master.zip)
+2. go to your admin panel and navigate to Extensiosn -> Manage and "Upload Package File" tab.
+3. choose the downloaded .zip file and click "Upload & Install"
+4. navigate to the plugin manager Extensiosn -> Plugins and search for "Content - ACF Article Custom Field"
+5. enable the plugin and select the joomla category you need apply the custom field to
+6. create a new article and select the category
+7. save the article
+8. a new tab called "Custom field" appears with the custom field
+
+At this point, you are able to save the custom field value in the  
--- acf.php
+++ acf.php
@@ -6,34 +6,64 @@
  */
 
 // Protect from unauthorized access
-defined ('_JEXEC') or die ('Restricted access');
+defined ( '_JEXEC' ) or die ( 'Restricted access' );
 
-class plgContentAcf extends JPlugin 
-{
+class plgContentAcf extends JPlugin {
 	protected $autoloadLanguage = true;
 
 	function onContentPrepareForm($form, $data)
 	{
 		$app = JFactory::getApplication();
-
-		if ($app->isAdmin())
-		{
-			$option = $app->input->get('option');
-			$view   = $app->input->get('view');
-
-			if ($option === 'com_content' && $view === 'article')
+		$option = $app->input->get('option');
+		switch($option) {
+			case 'com_content':
+			if ($app->isAdmin())
 			{
+				JForm::addFormPath(__DIR__ . '/forms');
+				$form->loadFile('content', false);
+
 				$model = JModelLegacy::getInstance('Article', 'ContentModel');
 				$catid = $model->getItem($app->input->get('id'))->catid;
 
-				if ($this->params->get('category') === $catid)
-				{
-					JForm::addFormPath(__DIR__ . '/forms');
-					$form->loadFile('content', false);
-				}
-			}
+				$pluginCatId = $this->params->get('category');
 
+				$document = JFactory::getDocument();
+				$document->addScriptDeclaration('
+				jQuery( document ).ready(function() {
+					var selectedCategory = jQuery("#jform_catid").val(),
+							catId = ' . $catid . ',
+							pluginCat = ' . $pluginCatId . ',
+							fields = jQuery("#attrib-acf .controls"),
+							labels = jQuery("#attrib-acf .control-label"),
+							warningMessage = jQuery("#attrib-acf .control-group .acf_warningMessage");
+
+
+					jQuery("#jform_catid").change(function() {
+						selectedCategory = jQuery("#jform_catid").val();
+						if(selectedCategory != pluginCat){
+							fields.hide();
+							labels.hide();
+							warningMessage.show();
+						} else {
+							fields.show();
+							labels.show();
+							warningMessage.hide();
+						}
+					});
+
+					if(selectedCategory != pluginCat){
+						fields.hide();
+						labels.hide();
+					} else {
+						warningMessage.hide();
+					}
+
+				});
+				');
+			}
 			return true;
 		}
+		return true;
 	}
 }
+?>
